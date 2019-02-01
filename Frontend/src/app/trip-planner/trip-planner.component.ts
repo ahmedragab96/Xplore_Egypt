9cbb6812ac58d7de1d5721e022802c7b053203cb
Ahmed Ebeid <a.ebeid@hotmail.com>
2019-02-01 23:00:48 +0200

    Name issue solved

--- Frontend/src/app/trip-planner/trip-planner.component.ts
+++ Frontend/src/app/trip-planner/trip-planner.component.ts
@@ -10,6 +10,14 @@ export class TripPlannerComponent implements OnInit {
   constructor() { }
  
 
+  trips =[
+    { "name": "Trip 1", "selected": false, "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit," },
+    { "name": "Trip 2", "selected": false, "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { "name": "Trip 3", "selected": false, "description": "Sed ut perspiciatis unde omnis iste natus error sit vol?" },
+    { "name": "Trip 4", "selected": false, "description": "On the other hand, we denounce with righteous indignati pains to avoid worse pains." },
+    { "name": "Trip 5", "selected": false, "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit dui, a pretium elit lobortis pretium." }]
+
+
   ngOnInit() {
   }
   
