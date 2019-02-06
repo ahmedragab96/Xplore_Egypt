21bd1c061c17b1cb2f6b1f0344f6de42bdfef066
ahmedragab96 <hamadar1996@gmail.com>
2019-02-06 18:49:39 +0200

    Add get_trips Api

--- Backend/express.js
+++ Backend/express.js
@@ -5,12 +5,14 @@ var bodyParser = require('body-parser');
 const app = express();
 
 var UserRoutes = require('./routes/user');
+var TripsRoutes = require('./routes/trips')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
 
 
 app.use('/users', UserRoutes);
+app.use('/trips',  TripsRoutes);
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
---
+++ Backend/routes/trips.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var trips = express.Router();
+const db = require('../app/config/db');
+
+trips.get('/getall', (req , res , next) => {
+    
+    let query = "SELECT * FROM trips";
+
+    db.query(query , function (error , results , fields) {
+        if (error) throw error ;
+
+        res.json(results);
+    });
+});
+
+
+module.exports = trips;
