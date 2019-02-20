ae939dfbd17626bf0852d2b6355d1606eccebeba
ahmedragab96 <hamadar1996@gmail.com>
2019-02-21 01:47:43 +0200

    API for retrieving user's plan

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -108,4 +108,22 @@ users.post('/addplan' , function(req , res , next ){
     
 });
 
+users.post('/getplan' , function(req , res , next ){
+  
+  const userid = req.query.id;
+  
+  db.query("SELECT trips FROM users WHERE ID = ?", [userid] , function (error , results , fields){
+
+    if (error) throw err ;
+    
+    const plan = JSON.parse(results[0].trips);
+    
+    res.status(200).json({
+      plan
+    })
+    
+  });
+    
+});
+
 module.exports = users;
