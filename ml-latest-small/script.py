2564653862706da346d41bf7cde3a8754b3aa854
maryamnabil <mariamnabil125@gmail.com>
2019-03-20 16:44:47 +0200

    adding files of trips - hotels -rest

--- ml-latest-small/script.py
+++ ml-latest-small/script.py
@@ -12,14 +12,31 @@ import json
 # f = open( 'new.json', 'w')  
 # f.write(out)  
 # print("JSON saved!")  
-input_file = open ('new.json', encoding='utf-8-sig')
-json_array = json.load(input_file)
-for x in json_array:
-	list_=x['genres'].split('|')
-	x['type']=list_[0]
-	x['price_cateogry']=list_[1]
-	x['keywords']=json.dumps(list_[2:])
-	x.pop('genres', None)
+# input_file = open ('new.json', encoding='utf-8-sig')
+# json_array = json.load(input_file)
+# for x in json_array:
+# 	list_=x['genres'].split('|')
+# 	x['type']=list_[0]
+# 	x['price_cateogry']=list_[1]
+# 	x['keywords']=json.dumps(list_[2:])
+# 	x.pop('genres', None)
 
-with open('clean.json', 'w') as outfile:
-    json.dump(json_array, outfile,indent=4)
+# with open('clean.json', 'w') as outfile:
+#     json.dump(json_array, outfile,indent=4)
+
+all_trips = open ('all-trips.json', encoding='utf-8-sig')
+Trips = open ('Trips.json', encoding='utf-8-sig')
+json_array_all_trips = json.load(all_trips)
+json_array_Trips = json.load(Trips)
+for x in json_array_all_trips:
+	for y in json_array_Trips:
+		if x['title']==y['title']:
+			x.append(y['itemid'])
+
+
+with open('trial.json', 'w') as outfile:
+    json.dump(json_array_all_trips, outfile,indent=4)
+
+
+
+
