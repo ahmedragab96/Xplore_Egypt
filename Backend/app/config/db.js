2c27e32b7f798ca9c4410f3cd4c7cd3e016cc597
maryamnabil <mariamnabil125@gmail.com>
2019-04-16 00:32:56 +0200

    create post

--- Backend/app/config/db.js
+++ Backend/app/config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const db = mysql.createConnection ({
 
-  host: "remotemysql.com",
-  user: "XBVQWXBf4L",
-  password: "aaYdZBVX6w",
-  database: "XBVQWXBf4L"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 db.connect((err) => {
   if (err) {
@@ -17,3 +17,23 @@ db.connect((err) => {
 
 
 module.exports = db ;
+
+// const mysql = require('mysql');
+// require('dotenv').config();
+
+// const db = mysql.createConnection ({
+
+//   host: "remotemysql.com",
+//   user: "XBVQWXBf4L",
+//   password: "aaYdZBVX6w",
+//   database: "XBVQWXBf4L"
+// });
+// db.connect((err) => {
+//   if (err) {
+//       throw err;
+//   }
+//   console.log('Connected to database');
+// });
+
+
+// module.exports = db ;
