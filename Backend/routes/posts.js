6f5e298a95690512ee540e016eb4d79b338b36fd
ahmedragab96 <hamadar1996@gmail.com>
2019-05-06 22:40:08 +0200

    delete logs and edit format

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -4,29 +4,29 @@ const db = require('../app/config/db');
 const AuthCheck = require("../middleware/check-auth")
 
 // get all posts from posts table
-posts.get('/getall', (req , res , next) => {
+posts.get('/getall', (req, res, next) => {
 
     let query = "SELECT * FROM posts";
 
 
-    db.query(query , function (error , results , fields) {
-        if (error) throw error ;
+    db.query(query, function (error, results, fields) {
+        if (error) throw error;
         res.json(results);
     });
 });
 
 // post a post in posts table
-posts.post('/postapost' , function (req , res , next ) {
+posts.post('/postapost', function (req, res, next) {
     const title = req.body.title;
     const body = req.body.body;
     const date = req.body.date;
     const upVoting = req.body.upVoting;
     const downVoting = req.body.downVoting;
     const userID = req.body.userID;
- 
+
     let query = "INSERT INTO `posts` (title, body, date, upVoting , downVoting , userID) VALUES ('" +
-                            title + "', '" + body + "','" + date + "','" + upVoting + "','" + downVoting + "','" + userID + "')";
-                            
+        title + "', '" + body + "','" + date + "','" + upVoting + "','" + downVoting + "','" + userID + "')";
+
     db.query(query, (err, result) => {
         if (err) {
             console.log(err)
@@ -35,23 +35,23 @@ posts.post('/postapost' , function (req , res , next ) {
         else {
             res.status(200).json({
                 result
-              })
-        }  
+            })
+        }
     });
 });
 
 //delete a post from posts table 
-posts.delete('/deletepost', function (req , res , next ){
+posts.delete('/deletepost', function (req, res, next) {
     const postID = req.body.postID;
     let query = "DELETE FROM posts WHERE postID = ?";
-    db.query(query, [postID] ,(err, result) => {
+    db.query(query, [postID], (err, result) => {
         if (err) {
             return res.status(500).send(err);
         }
         else {
             res.status(200).json({
                 result
-              })
+            })
         }
     });
 });
@@ -59,13 +59,13 @@ posts.delete('/deletepost', function (req , res , next ){
 
 
 // update a post from posts table
-posts.put('/updatepost', function (req , res , next ){
+posts.put('/updatepost', function (req, res, next) {
     const postID = req.body.postID;
     const title = req.body.title;
     const body = req.body.body;
-    if(req.body.title && !req.body.body){ //if title onlyyyyyyyyy
+    if (req.body.title && !req.body.body) { //if title onlyyyyyyyyy
         let query = "UPDATE posts SET title = ? WHERE postID = ? ";
-        db.query(query, [title, postID],(err, result) => {
+        db.query(query, [title, postID], (err, result) => {
             console.log("goa l query");
             if (err) {
                 return res.status(500).send(err);
@@ -73,46 +73,46 @@ posts.put('/updatepost', function (req , res , next ){
             else {
                 res.status(200).json({
                     result
-                  })
+                })
             }
         });
     }
-    else if(req.body.body && !req.body.title){ //if body onlyyyyyyyyy
+    else if (req.body.body && !req.body.title) { //if body onlyyyyyyyyy
         let query = "UPDATE posts SET body = ? WHERE postID = ? ";
-        db.query(query, [body, postID] ,(err, result) => {
+        db.query(query, [body, postID], (err, result) => {
             if (err) {
                 return res.status(500).send(err);
             }
             else {
                 res.status(200).json({
                     result
-                  })
+                })
             }
         });
     }
-    else{ //if both body and title
+    else { //if both body and title
         let query = "UPDATE posts SET title = ?, body = ? WHERE postID = ? ";
-        db.query(query, [title, body, postID] ,(err, result) => {
+        db.query(query, [title, body, postID], (err, result) => {
             if (err) {
                 return res.status(500).send(err);
             }
             else {
                 res.status(200).json({
                     result
-                  })
-            } 
+                })
+            }
         });
     }
 });
 
 // get all posts from posts table for specific user
-posts.get('/getuserposts', (req , res , next) => {
+posts.get('/getuserposts', (req, res, next) => {
 
     const userID = req.body.userID;
     let query = "SELECT * FROM posts WHERE userID = ?";
 
-    db.query(query ,[userID], function (error , results , fields) {
-        if (error) throw error ;
+    db.query(query, [userID], function (error, results, fields) {
+        if (error) throw error;
         res.json(results);
     });
 });
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -16,115 +16,113 @@ const MIME_TYPE_MAP = {
 };
 
 const storage = multer.diskStorage({
-  destination: (req , file , cb) => {
+  destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error ("Invalid image type");
+    let error = new Error("Invalid image type");
     if (isValid) {
       error = null;
     }
-    cb(null , "../images");
+    cb(null, "../images");
   },
-  filename: (req, file , cb) => {
+  filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
     const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null , name + '-' + Date.now() + '.' + ext);
+    cb(null, name + '-' + Date.now() + '.' + ext);
   }
 });
 
 //REGISTER
 
-users.post('/register' , multer({storage: storage}).single("image") , function (req , res , next ) {
-   const fname = req.body.fname;
-   const lname = req.body.lname;
-   const email = req.body.email;
-   const password = req.body.password;
-   const DoB = req.body.DOB;
-   const gender = req.body.gender;
-   const nationality = req.body.nationality;
+users.post('/register', multer({ storage: storage }).single("image"), function (req, res, next) {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const password = req.body.password;
+  const DoB = req.body.DOB;
+  const gender = req.body.gender;
+  const nationality = req.body.nationality;
 
-   bcrypt.hash(password, saltRounds, function(err, hash) {
-     let query = "INSERT INTO `users` (first_name, last_name, email, password , DoB , gender , nationality) VALUES ('" +
-                           fname + "', '" + lname + "','" + email + "','" + hash + "','" + DoB + "','" + gender + "', '" + nationality + "')";
-       db.query(query, (err, result) => {
-           if (err) {
-               return res.status(500).send(err);
-           }
-           db.query("SELECT LAST_INSERT_ID() as user_id", function (error , results , fields){
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    let query = "INSERT INTO `users` (first_name, last_name, email, password , DoB , gender , nationality) VALUES ('" +
+      fname + "', '" + lname + "','" + email + "','" + hash + "','" + DoB + "','" + gender + "', '" + nationality + "')";
+    db.query(query, (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      db.query("SELECT LAST_INSERT_ID() as user_id", function (error, results, fields) {
 
-              if (error) throw error ;
+        if (error) throw error;
 
-              const user_id = results[0];
+        const user_id = results[0];
 
-               let token = jwt.sign({email : email , userId : user_id} , 'secret' , {
-                 expiresIn : '1h'
-               })
-               res.json ({token : token});
-          })
-     })
+        let token = jwt.sign({ email: email, userId: user_id }, 'secret', {
+          expiresIn: '1h'
+        })
+        res.json({ token: token });
+      })
+    })
+  });
+
 });
 
-});
+users.post('/login', function (req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
 
-users.post('/login' , function (req , res , next ) {
-    const email = req.body.email;
-    const password = req.body.password;
+  db.query("SELECT ID , email , password FROM users WHERE email = ?", [email], function (error, results, fields) {
 
-  db.query("SELECT ID , email , password FROM users WHERE email = ?",  [email] , function (error , results , fields){
+    if (error) throw error;
 
-      if (error) throw error;
+    if (results.length === 0) {
+      return res.json({ error: "User doesnot exist!" })
+    }
+    else {
+      bcrypt.compare(password, results[0].password, (err, respond) => {
 
-      if (results.length === 0)
-        {
-          return res.json({error : "User doesnot exist!"})
+        if (!respond) {
+          return res.json({ error: "Password is incorrect!" });
         }
-      else
-        {
-          bcrypt.compare(password , results[0].password , (err, respond) => {
-            
-            if (!respond){
-              return res.json({error: "Password is incorrect!"});
-            }
-            if(respond){
-              console.log(results)
-            let token = jwt.sign({email : email , userId : results[0].ID} , 'secret' , {
-            expiresIn : '1h'
-            })
-            res.json({
-              token : token,
-              expireIn: 3600
-            });
-            }
+        if (respond) {
+          console.log(results)
+          let token = jwt.sign({ email: email, userId: results[0].ID }, 'secret', {
+            expiresIn: '1h'
           })
+          res.json({
+            token: token,
+            expireIn: 3600
+          });
         }
-    })
+      })
+    }
+  })
 });
 
-users.get('/getdata' , function(req , res , next ){
-  
-  const userid = req.query.id ;
-  
-  db.query("SELECT * FROM users WHERE ID = ?",  [userid] , function (error , results , fields){
+users.get('/getdata', function (req, res, next) {
 
-      res.json({
-        results
-      });
-    })
-    
+  const userid = req.query.id;
+
+  db.query("SELECT * FROM users WHERE ID = ?", [userid], function (error, results, fields) {
+
+    res.json({
+      results
+    });
+  })
+
 });
 
-users.get('/getall' , function(req , res , next ){
-  
-  db.query("SELECT * FROM users", function (error , results , fields){
+users.get('/getall', function (req, res, next) {
 
-      res.json({
-        results
-      });
-    })
-    
+  db.query("SELECT * FROM users", function (error, results, fields) {
+
+    res.json({
+      results
+    });
+  })
+
 });
-users.put('/edit/:id' ,(req , res , next )=>{
+users.put('/edit/:id', (req, res, next) => {
 
-  let id=req.params.id;
+  let id = req.params.id;
   let fname = req.body.fname;
   let lname = req.body.lname;
   let email = req.body.email;
@@ -132,85 +130,84 @@ users.put('/edit/:id' ,(req , res , next )=>{
   let gender = req.body.gender;
   let nationality = req.body.nationality;
 
-  if(req.body.password != undefined)
-   {
+  if (req.body.password != undefined) {
     let password = req.body.password;
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-      let query = "UPDATE `users` SET `first_name` = '"+fname +"' , `last_name` ='"+lname+"' , `email` = '"+email+
-      "' , `password` = '"+password+"' , `DoB` = '"+DOB+"' , `gender` = '"+gender+"' , `nationality` ='"+nationality+"' WHERE ID = ?";
-      
-        db.query(query,[id], (err, result) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            res.send("edited successfully ");
-          
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      let query = "UPDATE `users` SET `first_name` = '" + fname + "' , `last_name` ='" + lname + "' , `email` = '" + email +
+        "' , `password` = '" + password + "' , `DoB` = '" + DOB + "' , `gender` = '" + gender + "' , `nationality` ='" + nationality + "' WHERE ID = ?";
+
+      db.query(query, [id], (err, result) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.send("edited successfully ");
+
       })
     });
 
   }
-  else{
-    
-    let query = "UPDATE `users` SET `first_name` = '"+fname +"' , `last_name` ='"+lname+"' , `email` = '"+email+
-    "' , `DoB` = '"+DOB+"' , `gender` = '"+gender+"' , `nationality` ='"+nationality+"' WHERE ID = ?";
-    
-      db.query(query,[id], (err, result) => {
-          if (err) {
-              return res.status(500).send(err);
-          }
-          res.send("edited successfully without password");
-        
+  else {
+
+    let query = "UPDATE `users` SET `first_name` = '" + fname + "' , `last_name` ='" + lname + "' , `email` = '" + email +
+      "' , `DoB` = '" + DOB + "' , `gender` = '" + gender + "' , `nationality` ='" + nationality + "' WHERE ID = ?";
+
+    db.query(query, [id], (err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      res.send("edited successfully without password");
+
     })
   }
 });
 
-users.delete('/delete/:id',(req,res)=>{
-  let id=req.params.id;
-  let query ="DELETE FROM users WHERE ID = ?";
-  db.query(query, [id] , function (error , results , fields){
+users.delete('/delete/:id', (req, res) => {
+  let id = req.params.id;
+  let query = "DELETE FROM users WHERE ID = ?";
+  db.query(query, [id], function (error, results, fields) {
 
-    if (error) throw err ;
-    res.send("successfully deleted user "+id);
-    
+    if (error) throw err;
+    res.send("successfully deleted user " + id);
+
   });
 });
 // Function needs 2 parameters as body request (id , plan)
-users.post('/addplan' , function(req , res , next ){
+users.post('/addplan', function (req, res, next) {
   const userid = req.body.id;
   console.log(userid);
-  
+
   const plan = JSON.stringify(req.body.plan);
   console.log(plan);
-  
-  db.query("UPDATE users SET trips = ? WHERE ID = ?", [plan , userid] , function (error , results , fields){
 
-    if (error) throw err ;
-    
+  db.query("UPDATE users SET trips = ? WHERE ID = ?", [plan, userid], function (error, results, fields) {
+
+    if (error) throw err;
+
     res.status(200).json({
       results
     })
-    
+
   });
-  
-    
+
+
 });
 
-users.get('/getplan' , function(req , res , next ){
-  
-  const userid = req.query.id;
-  
-  db.query("SELECT trips FROM users WHERE ID = ?", [userid] , function (error , results , fields){
+users.get('/getplan', function (req, res, next) {
 
-    if (error) throw err ;
-    
+  const userid = req.query.id;
+
+  db.query("SELECT trips FROM users WHERE ID = ?", [userid], function (error, results, fields) {
+
+    if (error) throw err;
+
     const plan = JSON.parse(results[0].trips);
-    
+
     res.status(200).json({
       plan
     })
-    
+
   });
-    
+
 });
 
 module.exports = users;
