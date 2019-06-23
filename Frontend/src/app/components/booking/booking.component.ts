f0ce833961a030f3a016d333177a1a395c255e49
NourhanMS <nourhan_195@hotmail.com>
2019-06-23 02:22:32 +0200

    added trip advisor to booking

--- Frontend/src/app/components/booking/booking.component.ts
+++ Frontend/src/app/components/booking/booking.component.ts
@@ -13,7 +13,14 @@ export class BookingComponent implements OnInit {
   
   }
   book(){
-    var cx = '010207221007117448908:quimk_k6jty';
+    // var cx = '010207221007117448908:quimk_k6jty';
+    // var gcse = document.createElement('script');
+    // gcse.type = 'text/javascript';
+    // gcse.async = true;
+    // gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    // var s = document.getElementsByTagName('script')[0];
+    // s.parentNode.insertBefore(gcse, s);
+    var cx = '010207221007117448908:suxic_nlyoe';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
